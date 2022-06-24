792654beda8d4916aa77cbf833b7d5717c524e96
@esson <55195336+githubesson@users.noreply.github.com>
2022-06-24 15:21:59 +0200

    master

--- walletgen/walletgen.js
+++ walletgen/walletgen.js
@@ -1,21 +1,34 @@
 const solweb3 = require('@solana/web3.js');
 const prompt = require('prompt-sync')();
 const fs = require('fs');
-var base58 = require("bs58");
+var base58 = require('bs58');
 
+//establish connection with rpc
 const Solana = new solweb3.Connection(
     "https://api.mainnet-beta.solana.com"
 );
 
-console.log(`walletgen: 1. gen wallets`);
-const walletnum = prompt("How many wallets do you want to generate?: ");
+console.log(`esson walletgen`);
+const walletnum = prompt("How many wallets/keypairs do you want to generate?: ");
 const save = prompt("Save to txt? (yes/no): ");
+
 for(i = 0; i < walletnum; i++){
+
+    //gen wallet/keypair
+
     const keypair = solweb3.Keypair.generate();
+
+    //turn private key uint8 array to bs58 string
+
     decode = base58.encode(keypair.secretKey)
+
     console.log(`Public Key ${i+1}:`, keypair.publicKey.toString());
     console.log(`Private Key ${i+1}:`, decode);
+
     let keypairsave = keypair.publicKey.toString() + ", " + decode + "\n"
+
+    //save to file
+
     if(save == "yes"){
         fs.appendFile('keypairs.txt', keypairsave, err => {
             if (err) {
@@ -23,5 +36,8 @@ for(i = 0; i < walletnum; i++){
               return
             }
         })
+        console.log(`saved ${i+1} keypair`);
     }
+
+    else console.log('done');
 }
