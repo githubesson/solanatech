1016dabd91df368775440aa884977cece312da33
@esson <55195336+githubesson@users.noreply.github.com>
2022-06-24 15:08:50 +0200

    master

---
+++ walletgen.js
@@ -0,0 +1,27 @@
+const solweb3 = require('@solana/web3.js');
+const prompt = require('prompt-sync')();
+const fs = require('fs');
+var base58 = require("bs58");
+
+const Solana = new solweb3.Connection(
+    "https://holynode.com/"
+);
+
+console.log(`walletgen: 1. gen wallets`);
+const walletnum = prompt("How many wallets do you want to generate?: ");
+const save = prompt("Save to txt? (yes/no): ");
+for(i = 0; i < walletnum; i++){
+    const keypair = solweb3.Keypair.generate();
+    decode = base58.encode(keypair.secretKey)
+    console.log(`Public Key ${i+1}:`, keypair.publicKey.toString());
+    console.log(`Private Key ${i+1}:`, decode);
+    let keypairsave = keypair.publicKey.toString() + ", " + decode + "\n"
+    if(save == "yes"){
+        fs.appendFile('keypairs.txt', keypairsave, err => {
+            if (err) {
+              console.error(err)
+              return
+            }
+        })
+    }
+}
